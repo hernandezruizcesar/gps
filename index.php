17ffcd809923655739dcfa39702dca597cf52a3d
CayetanoHerreraLuisRicardo <luisfobia92@gmail.com>
2014-10-25 14:36:06 -0500

    subiendo archivos base :)

---
+++ index.php
@@ -0,0 +1,131 @@
+<?php $nombrepagina='Inicio'; ?>
+
+<!DOCTYPE html>
+<!--[if IE 8 ]><html class="ie ie8" lang="es"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="es"> <!--<![endif]-->
+<head>
+		<!-- Basic Page Needs
+	================================================== -->
+	<meta charset="utf-8">
+	<title><?PHP echo $nombrepagina;?></title>
+
+	<!-- Mobile Specific Metas
+	================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+	================================================== -->
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/colors/red.css" id="colors">
+
+	<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Java Script
+	================================================== -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery.flexslider.js"></script>
+	<script src="js/jquery.selectnav.js"></script>
+	<script src="js/jquery.twitter.js"></script>
+	<script src="js/jquery.modernizr.js"></script>
+	<script src="js/jquery.easing.1.3.js"></script>
+	<script src="js/jquery.contact.js"></script>
+	<script src="js/jquery.isotope.min.js"></script>
+	<script src="js/jquery.jcarousel.js"></script>
+	<script src="js/jquery.fancybox.min.js"></script>
+	<script src="js/jquery.layerslider.min.js"></script>
+	<script src="js/jquery.shop.js"></script>
+	<script src="js/custom.js"></script>
+</script>
+</head>
+<body>
+
+	<!-- Wrapper / Start -->
+	<div id="wrapper">
+		<!-- Header
+		================================================== -->
+		<div id="top-line"></div>
+
+		<!-- 960 Container -->
+		<div class="container">
+
+			<!-- Header -->
+			<header id="header">
+
+				<!-- Logo -->
+				<div class="sixteen columns">
+					<div id="logo">
+						<h1> Prueba</h1>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+			</header>
+			<!-- Header / End -->
+			<div class="clearfix"></div>
+			
+		</div>
+		<!-- 960 Container / End -->
+	
+	
+		<!-- Content
+		================================================== -->
+		<div id="content">
+		
+			<!-- 960 Container -->
+			<div class="container floated">
+
+				<div class="sixteen floated page-title">
+
+					<h2>Titulo</h2>
+
+				</div>
+
+			</div>
+			<!-- 960 Container / End -->
+			
+			<div class="container">
+			
+			</div>
+			
+			<div class="container">
+
+
+			</div><br><br>
+		</div>
+		<!-- Content / End -->
+	</div>
+	<!-- Wrapper / End -->
+	
+	<!-- Footer Bottom / Start  -->
+	<footer id="footer-bottom">
+
+		<!-- 960 Container -->
+		<div class="container">
+
+			<!-- Copyrights -->
+			<div class="ten columns">
+				<div class="copyright">
+					&copy <a href="#"> Copyright</a> 2014
+				</div>
+			</div>
+
+			<!-- Menu -->
+			<div class="six columns">
+				<nav id="sub-menu">
+					<ul>
+						<li><a href="#">Mapa del Sitio</a></li>
+						<li><a href="#">Contacto</a></li>
+					</ul>
+				</nav>
+			</div>
+
+		</div>
+		<!-- 960 Container / End -->
+
+	</footer>
+	<!-- Footer Bottom / End -->
+
+
+</body>
+</html>
